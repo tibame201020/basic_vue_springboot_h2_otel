8e2aa2107d695d8cc16d3f49d57d9ff3cfb69089
USER\jeffhsu <tibame201020@gmail.com>
2024-04-28 11:30:08 +0800

    bacis k6 script

---
+++ backend/k6/script.js
@@ -0,0 +1,10 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+export const options = {
+  vus: 10,
+  duration: '5s',
+};
+export default function () {
+  http.get('http://localhost:8089/api/publishApi');
+  sleep(1);
+}
